56427896f2849fe569b5637ac815d30fd8e8a726
Azro1 <azro1.development@gmail.com>
2023-07-20 09:18:52 +0100

    Add backend to secure/hide accuweather apikey - created package.json file and installed express, dotenv and cors packages - created function in App.js component to make request to backend api on page load using uesEffect

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import { useState, useEffect } from 'react';
 import './App.css';
 import AddCity from './components/AddCity';
 import Card from './components/Card';
 
+
 function App() {
+
+const [city, setCity] = useState([])
+
+  useEffect(() => {
+    // get weather from server
+    const cityWeather = async () => {
+      const response = await fetch("http://localhost:8000/city-conditions")
+      const data = await response.json()
+      setCity(data)
+    }
+    cityWeather()
+}, [])
+
   return (
     <div className="wrapper">
        <h1>Sky Explorer</h1>
